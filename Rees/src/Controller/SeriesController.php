e9206089e92a1bea180cea8a395d612de0ac9701
dbriend <donovan.briend@etu.u-bordeaux.fr>
2023-01-10 17:19:24 +0100

    page My series works

--- Rees/src/Controller/SeriesController.php
+++ Rees/src/Controller/SeriesController.php
@@ -74,36 +74,11 @@ class SeriesController extends AbstractController
     }
 
     #[Route(['/tracked'], name: 'app_series_tracked', methods: ['GET', 'POST'])]
-    public function tracked(EntityManagerInterface $entityManager, Request $page): Response
+    public function tracked(): Response
     {
-        $lastData = $entityManager
-            ->getRepository(Series::class)
-            ->findOneBy([], ['id' => 'desc']);
-        $page = $page->query->getInt('page', 1);
-            //if the number of the page is below 1, or above (last number of page), it will redirect to an error page
-        if($page<1){
-            $page = 1;
-        }
-        else if( $page > (($lastData->getId())/10)-1){
-            $page = $lastData->getId()/10 -1.4;
-        }
-        
-        $query = $entityManager->createQuery(
-            "SELECT s FROM App\Entity\Series s
-             INNER JOIN App\Entity\Genre g
-             ORDER BY s.id");
-        $posts = $this->paginate($query, $page);
-        $posts->setUseOutputWalkers(false);
-        $series = $posts->getIterator();
-
-        $limit = 10;
-        $maxPages = ceil($posts->count()/ $limit);
-        $thisPage = $page;
-
-        return $this->render('series/index.html.twig', [
-            'series' => $series,
-            'maxPages'=> $maxPages,
-            'thisPage' => $thisPage
+        $user = $this->getUser();
+        return $this->render('series/tracked/index.html.twig', [
+            'user' => $user
         ]);
     }
 
